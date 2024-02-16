e80a6a292d63212425b51e35c1cf4db0b78aaf83
flames239 <nam23996@gmail.com>
2024-02-16 19:45:01 +0700

    Update file Login_EditCV

--- cypress/support/page_objects/Login_EditCV.js
+++ cypress/support/page_objects/Login_EditCV.js
@@ -49,80 +49,93 @@ function LoginFalseAndLoginAgain() {
     cy.get('#resume_title').should('be.visible').clear().type('Fresher Automation Tester')
     cy.get('#t-resume-form').find('.btn-gradient').should('contain', 'Lưu Lại').click()
 
-    // Personal Information
-    cy.get('#personalinfo-section a').click()
-    cy.get('#lastname').clear().type('Hồ Quý Phương') // last name
-    cy.get('#firstname').clear().type('Ly') // first name
-    cy.get('#gender_f').click({ force: true }) // gender
 
-    cy.get('[name="birthday"]').click() // choose birthday
+    // Objective (Muc tieu nghe nghiep) Optional
+
+
+    // Career Information (Thông tin nghề nghiệp)
+    cy.get('#widget-18 a').should('be.visible').click(); // click on edit button
+    // popup form appearance
+
+    // worktype (Hình Thức Làm Việc)
+    cy.get('#chkResumeType_1').click({ force: true });
+    cy.get('#chkWorkHome').click({ force: true });  // Flexible work (Phương thức làm việc WFH) - Not Required
+
+    cy.get('[name="level_id"]').select('Nhân viên'); // Level ID (Cấp bậc mong muốn)
+    cy.get('#salary_unit').select('USD');
+    cy.get('#salary_from').clear().type('800');
+    cy.get('#salary_to').clear().type('1000');
+    //cy.get('#salary_unit').select('Thỏa thuận'); // Expected Salary 
+    //cy.get('#salary_unit').select('VNĐ');
+
+    // Click on the dropdown to open it
+    cy.get('#INDUSTRY_ID_chosen').click()
+
+    // Click on the options while holding the Ctrl (or Command) key
+    cy.get('#INDUSTRY_ID_chosen').contains('CNTT / Phần mềm').click({ ctrlKey: true })
+    cy.get('#INDUSTRY_ID_chosen').contains('Quản lý chất lượng (QA/QC)').click({ ctrlKey: true })
+
+    // After selecting the options, click outside the dropdown to close it
+    cy.get('body').click({ force: true })
+
+    cy.get('#select_location_id_3').select('Hồ Chí Minh'); // Expected WOrk location
+    cy.get('#desired-form').contains('Thêm').click(); // Optional add more desired locations work place
+    cy.get('#select_location_id_1').select('Đà Nẵng');
+
+    cy.get('#desired-form button.btn-gradient').click();
+
+    // Click on form Experience
+    cy.get('#widget-15').find('span').contains('Thêm mới').click()
+
+
+
+
+
+
+
+
+
+    // Personal Information
+    cy.get('#personalinfo-section a').click();
+    cy.get('#lastname').clear().type('Hồ Quý Phương'); // last name
+    cy.get('#firstname').clear().type('Ly'); // first name
+    cy.get('#gender_f').click({ force: true }); // gender
+
+    cy.get('[name="birthday"]').click(); // choose birthday
     // declare variables for increment and decrement buttons for birthdays
 
     for (var i = 0; i < 2; i++) {
         cy.get('.dtpicker-content')
             .find('a[class="dtpicker-compButton increment dtpicker-compButtonEnable"]')
             .eq(0)
-            .click()
+            .click();
         cy.get('.dtpicker-content')
             .find('a[class="dtpicker-compButton decrement dtpicker-compButtonEnable"]')
             .eq(1)
-            .click()
+            .click();
         cy.get('.dtpicker-content')
             .find('a[class="dtpicker-compButton decrement dtpicker-compButtonEnable"]')
             .eq(2)
-            .click()
+            .click();
     }
+
     cy.get('.dtpicker-content')
         .find('a[class="dtpicker-button dtpicker-buttonSet"]')
-        .click()
+        .click();
 
-    cy.get('#mobile').clear().type('0984561987') // Mobile
-    cy.get('#slnationality').select('Người Việt Nam') // Nationallity
-    cy.get('#slcountry').select('Việt Nam') // International
-    cy.get('#slcity').select("Hà Nội") // Country
-    cy.get('#sldistrict').select("Quận Hoàng Mai") // District
-    cy.get('input[name="address"]').clear().type('123 Ly Thuong Kiet') // Address
-    cy.get('button')
-        .parents('#personalInfoForm')
-        .contains('Lưu lại')
-        .click()
+    cy.get('#mobile').clear().type('0984561987'); // Mobile
+    cy.get('#slnationality').select('Người Việt Nam'); // Nationallity
+    cy.get('#slcountry').select('Việt Nam'); // International
+    cy.get('#slcity').select("Hà Nội"); // Country
+    cy.get('#sldistrict').select("Quận Hoàng Mai"); // District
+    cy.get('input[name="address"]').clear().type('123 Ly Thuong Kiet'); // Address
+
+    // Click the "Lưu lại" button using contains() method with a more specific selector
+    cy.get('form#personalInfoForm button[type="button"]').click();
 
     // close popup cause after clicking save but the popup form not closed
     cy.get('button[class="fancybox-button fancybox-close-small"]').click()
 
-
-    // Objective (Muc tieu nghe nghiep) Optional
-
-
-    // Career Information (Thông tin nghề nghiệp)
-    cy.get('#widget-18 a').should('be.visible').click() // click on edit button
-    // popup form appearance
-
-    // worktype (Hình Thức Làm Việc)
-    cy.get('#chkResumeType_1').click()
-    cy.get('#chkWorkHome').click().should('be.checked')  // Flexible work (Phương thức làm việc WFH) - Not Required
-
-    cy.get('[name="level_id"]').select('Nhân viên') // Level ID (Cấp bậc mong muốn)
-    cy.get('#salary_unit').select('USD')
-    cy.get('#salary_from').clear().type('800')
-    cy.get('#salary_to').clear().type('1000')
-        //cy.get('#salary_unit').select('Thỏa thuận') // Expected Salary 
-    //cy.get('#salary_unit').select('VNĐ')
-
-    cy.get('#INDUSTRY_ID').select('["CNTT - Phần mềm","Quản lý chất lượng (QA/QC)"]') // industry
-
-    cy.get('#select_location_id_3').select('Hồ Chí Minh') // Expected WOrk location
-    cy.get('#select_district_id_3').select('Quận 3')
-    cy.get('#desired-form').contains('Thêm').click() // Optional add more desired locations work place
-    cy.get('#select_location_id_1').select('Đà Nẵng')
-    cy.get('#select_district_id_1').select('Quận Sơn Trà')
-
-    cy.get('#OUTCOUNTRY_ID').select('["Canada","Hoa Kỳ","Nhật Bản"]') // different Country
-    cy.get('#BENEFIT_ID').select('["Chế độ bảo hiểm","Chế độ thưởng","Chăm sóc sức khỏe"]')
-    cy.get('button').parents('#desired-form').contains('Lưu lại').click()
-
-    // Click on form Experience
-    cy.get('#widget-15').find('span').contains('Thêm mới').click()
 
 }
 
