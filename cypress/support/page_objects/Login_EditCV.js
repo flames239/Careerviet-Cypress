597ab7ed0928dcc58a8f1574571b262dc3a45745
flames239 <nam23996@gmail.com>
2024-02-18 21:00:06 +0700

    continue update file Login_EditCV

--- cypress/support/page_objects/Login_EditCV.js
+++ cypress/support/page_objects/Login_EditCV.js
@@ -32,7 +32,6 @@ function LoginFalseAndLoginAgain() {
     cy.get('#password').should('be.visible').clear().type('987654321')
     cy.get('#submit_login').should('be.visible').click({ force: true })
 
-
     // skip popup tutorial
     cy.get('#welcome-to').contains('Bỏ qua').should('be.visible').click()
 
@@ -43,58 +42,17 @@ function LoginFalseAndLoginAgain() {
     cy.get('[class="container-ckb"]').click()
     cy.get('#popup-modal').contains('Đóng').should('be.visible').click()
 
-    // Create or edit CV Profile
+    // Objective (Muc tieu nghe nghiep) Optional
+}
+
+function TitleResume() {
     // Title Resume
     cy.get('#t-resume-section a').click()
     cy.get('#resume_title').should('be.visible').clear().type('Fresher Automation Tester')
     cy.get('#t-resume-form').find('.btn-gradient').should('contain', 'Lưu Lại').click()
+}
 
-
-    // Objective (Muc tieu nghe nghiep) Optional
-
-
-    // Career Information (Thông tin nghề nghiệp)
-    cy.get('#widget-18 a').should('be.visible').click(); // click on edit button
-    // popup form appearance
-
-    // worktype (Hình Thức Làm Việc)
-    cy.get('#chkResumeType_1').click({ force: true });
-    cy.get('#chkWorkHome').click({ force: true });  // Flexible work (Phương thức làm việc WFH) - Not Required
-
-    cy.get('[name="level_id"]').select('Nhân viên'); // Level ID (Cấp bậc mong muốn)
-    cy.get('#salary_unit').select('USD');
-    cy.get('#salary_from').clear().type('800');
-    cy.get('#salary_to').clear().type('1000');
-    //cy.get('#salary_unit').select('Thỏa thuận'); // Expected Salary 
-    //cy.get('#salary_unit').select('VNĐ');
-
-    // Click on the dropdown to open it
-    cy.get('#INDUSTRY_ID_chosen').click()
-
-    // Click on the options while holding the Ctrl (or Command) key
-    cy.get('#INDUSTRY_ID_chosen').contains('CNTT / Phần mềm').click({ ctrlKey: true })
-    cy.get('#INDUSTRY_ID_chosen').contains('Quản lý chất lượng (QA/QC)').click({ ctrlKey: true })
-
-    // After selecting the options, click outside the dropdown to close it
-    cy.get('body').click({ force: true })
-
-    cy.get('#select_location_id_3').select('Hồ Chí Minh'); // Expected WOrk location
-    cy.get('#desired-form').contains('Thêm').click(); // Optional add more desired locations work place
-    cy.get('#select_location_id_1').select('Đà Nẵng');
-
-    cy.get('#desired-form button.btn-gradient').click();
-
-    // Click on form Experience
-    cy.get('#widget-15').find('span').contains('Thêm mới').click()
-
-
-
-
-
-
-
-
-
+function PersonalInfo() {
     // Personal Information
     cy.get('#personalinfo-section a').click();
     cy.get('#lastname').clear().type('Hồ Quý Phương'); // last name
@@ -135,8 +93,71 @@ function LoginFalseAndLoginAgain() {
 
     // close popup cause after clicking save but the popup form not closed
     cy.get('button[class="fancybox-button fancybox-close-small"]').click()
+}
 
+function CareerInfo(){
+      // Career Information (Thông tin nghề nghiệp)
+      cy.get('#widget-18 a').should('be.visible').click(); // click on edit button
+      // popup form appearance
+  
+      // worktype (Hình Thức Làm Việc)
+      //cy.get('#chkResumeType_1').click({ force: true });
+      cy.get('#chkWorkHome').click({ force: true });  // Flexible work (Phương thức làm việc WFH) - Not Required
+  
+      cy.get('[name="level_id"]').select('Nhân viên'); // Level ID (Cấp bậc mong muốn)
+      cy.get('#salary_unit').select('USD');
+      cy.get('#salary_from').clear().type('800');
+      cy.get('#salary_to').clear().type('1000');
+      //cy.get('#salary_unit').select('Thỏa thuận'); // Expected Salary 
+      //cy.get('#salary_unit').select('VNĐ');
+  
+      // Click on the dropdown to open it
+      cy.get('#INDUSTRY_ID_chosen').click()
+  
+      // Click on the options while holding the Ctrl (or Command) key
+      if (cy.get('#INDUSTRY_ID_chosen') === 0) {
+          cy.get('#INDUSTRY_ID_chosen').contains('CNTT / Phần mềm').click({ ctrlKey: true })
+          cy.get('#INDUSTRY_ID_chosen').contains('Quản lý chất lượng (QA/QC)').click({ ctrlKey: true })
+  
+          // After selecting the options, click outside the dropdown to close it
+          cy.get('body').click({ force: true })
+      } else {
+          cy.get('[class="search-choice"] a[class="search-choice-close"]').eq(0).click()
+          cy.get('#INDUSTRY_ID_chosen').click()
+          cy.get('#INDUSTRY_ID_chosen').contains('Bán lẻ / Bán sỉ').click({ ctrlKey: true })
+  
+          // After selecting the options, click outside the dropdown to close it 
+          cy.get('body').click({ force: true })
+      }
+  
+      cy.get('#select_location_id_3').select('Thái Nguyên'); // Expected Work location
+      //cy.get('#desired-form').contains('Thêm').click(); // Optional add more desired locations work place
+      cy.get('#select_location_id_1').select('Hà Nam');
+  
+      cy.get('#desired-form button.btn-gradient').click();
+}
 
+function WorkExperience() {
+    // Click on form Experience
+    cy.get('#widget-15').find('span').contains('Thêm mới').click()
+    
+    cy.get('[name="rexp_title"]').clear().type("Quality Control Engineer") // Title - Vị trí / Chức Danh
+    cy.get('[name="rexp_company"]').clear().type("CJ Entertainment") // Company - Công ty
+    cy.get('[name="rexp_worktype"]').select("Nhân viên chính thức")// Work Permanent - Phuong Thuc Cong Viec
+    
+    //  Thoi gian lam viec - Start Date
+    cy.get('#job_startdate').click()
+    cy.get('.datepicker-months .table-condensed').find('th[class="prev"]').eq(0).click()
+    cy.get('.datepicker-months .table-condensed').find('span[class="month"]').contains("Th1").click()
+    // End Date
+    cy.get('#job_enddate').click()
+    cy.get('.datepicker-months .table-condensed').eq(1).find('span[class="month"]').contains("Th1").click()
+
+    // Description
+    cy.get('[name="rexp_workdesc"]').clear().type('Project: myLocal.vn\n Description: a super app to support end-user activation SIM, update information, change eSIM, change physical SIM and many function into this app meeting end-user needs for a typical telecom operator.\n Domain: Telecom \nPosition: Core Member Quality Control \nTeam Size: 10 personsMy Responsibilities: Join Grooming,Testcases Writing, Execute Test, Test Report Daily, Demo to Stakeholder, QC UAT, Join Runbook.')
+    
+    // Save
+    ct.get('#experience-form .button-save .btn-gradient').contains('Lưu Lại').click()
 }
 
 export class Login_EditCV {
@@ -148,6 +169,7 @@ export class Login_EditCV {
 
     LoginFalse_LoginAgain() {
         LoginFalseAndLoginAgain()
+        WorkExperience()
     }
 }
 
