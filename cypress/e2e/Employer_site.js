c0e31b0c65b99241721c93f8a0c5b0d288b76fc7
flames239 <nam23996@gmail.com>
2024-08-04 19:31:02 +0700

    update func JobOnExpire() and update test case for JobOnExpire()

--- cypress/e2e/Employer_site.js
+++ cypress/e2e/Employer_site.js
@@ -15,53 +15,75 @@ describe("Employer Site", () => {
         onNavigationPage.Contact();
     });
 
-    it("Login employer then direct to dashboard page and take some action", () => {
-        onLogin.LoginFunction("123", "123", "dingdong23996@gmail.com", "reborn2391996");
-        // onWorkingOnDB.WorkingOnDashBoard();
-    });
+    // it("Login employer then direct to dashboard page and take some action", () => {
+    //     onLogin.LoginFunction("123", "123", "dingdong23996@gmail.com", "reborn2391996");
+    //     // onWorkingOnDB.WorkingOnDashBoard();
+    // });
 
     it("Login employer then direct to postjob page and take some action", () => {
         onLogin.LoginFunction("123", "123", "dingdong23996@gmail.com", "reborn2391996");
         onWorkWithhManageJob.ClickManageJob();
         onWorkWithhManageJob.JobOnHiring(
-          "Quality Assurance / Quality Control",
-          "QAL123",
-          "650", // option value location 4, 8, 76, 64, 781, 281, 240, 241, 75, 650, 651, 62, 78, 26, 71, 50, 1042, 511, 900, 1064, 61, 67, 59, 19, 351, 39, 320, 31, 780, 18, 321, 901, 58, 77, 60, 1069, 1070, 1071, 1072
-          "1. Lập kế hoạch và chiến lược kiểm tra: \n Cộng tác với người quản lý dự án, nhà phát triển và các bên liên quan khác để hiểu yêu cầu của dự án và tạo kế hoạch kiểm tra toàn diện. \n Xác định mục tiêu, phạm vi và thời gian kiểm tra cho mỗi lần phát hành phần mềm. \n Phát triển các chiến lược thử nghiệm để đảm bảo phạm vi và hiệu quả thử nghiệm tối đa. \n 2. Thực hiện kiểm tra: \n Thực hiện nhiều loại thử nghiệm khác nhau, bao gồm thử nghiệm chức năng, hồi quy, tích hợp và hiệu suất. \n Thực hiện các trường hợp kiểm thử theo kế hoạch kiểm thử đã xác định và báo cáo mọi sai lệch hoặc khiếm khuyết được tìm thấy. \n Sử dụng các công cụ và tập lệnh kiểm tra tự động để hợp lý hóa các quy trình kiểm tra nếu có. \n Tiến hành thử nghiệm thăm dò để phát hiện các vấn đề không lường trước được và đảm bảo phạm vi thử nghiệm toàn diện.",
-          "Tốt nghiệp Đại học chuyên ngành Khoa học Máy tính, Kỹ thuật phần mềm hoặc lĩnh vực liên quan. \n Có ít nhất 3 năm kinh nghiệm về kiểm soát chất lượng phần mềm hoặc kiểm tra phần mềm \n Kiến thức vững chắc về các phương pháp, công cụ và phương pháp kiểm thử phần mềm tốt nhất. \n Có khả năng viết kế hoạch kiểm thử và trường hợp kiểm thử rõ ràng, ngắn gọn và toàn diện. \n Có kinh nghiệm với các công cụ tự động hóa thử nghiệm và ngôn ngữ kịch bản (ví dụ: Selenium, Python, v.v.). \n Kỹ năng phân tích và giải quyết vấn đề xuất sắc với sự chú ý sâu sắc đến từng chi tiết. \n Kỹ năng giao tiếp hiệu quả, cả bằng lời nói và văn bản, cùng với khả năng cộng tác với các nhóm đa chức năng. \n Khả năng làm việc độc lập và ưu tiên các nhiệm vụ một cách hiệu quả trong môi trường có nhịp độ nhanh.",
-          "https://www.youtube.com/watch?v=RDpcHAGZ0XI",
-          "https://www.youtube.com/watch?v=M8qQTbUgRfc",
-          "15,000,000",
-          "25,000,000",
-          "23", // From Age
-          "1", // select Exp
-          "2", // input Exp From
-          "6", // input Exp To
-          "3", // select Level_ID
-          "3", // select Degree_ID
-          "2 Months",
-          "Monday To Friday: 8h30 - 17h30",
-          "Learning skill from platform Udemy, Coursera",
-          "Friendly, Leader Manager Nice",
-          "Bonus Attractive 13th, Review Salary base on 6 month / year",
-          "With company rule",
-          "20 days / years"
+            "Quality Assurance / Quality Control",
+            "QAL123",
+            "650", // option value location 4, 8, 76, 64, 781, 281, 240, 241, 75, 650, 651, 62, 78, 26, 71, 50, 1042, 511, 900, 1064, 61, 67, 59, 19, 351, 39, 320, 31, 780, 18, 321, 901, 58, 77, 60, 1069, 1070, 1071, 1072
+            "1. Lập kế hoạch và chiến lược kiểm tra: \n Cộng tác với người quản lý dự án, nhà phát triển và các bên liên quan khác để hiểu yêu cầu của dự án và tạo kế hoạch kiểm tra toàn diện. \n Xác định mục tiêu, phạm vi và thời gian kiểm tra cho mỗi lần phát hành phần mềm. \n Phát triển các chiến lược thử nghiệm để đảm bảo phạm vi và hiệu quả thử nghiệm tối đa. \n 2. Thực hiện kiểm tra: \n Thực hiện nhiều loại thử nghiệm khác nhau, bao gồm thử nghiệm chức năng, hồi quy, tích hợp và hiệu suất. \n Thực hiện các trường hợp kiểm thử theo kế hoạch kiểm thử đã xác định và báo cáo mọi sai lệch hoặc khiếm khuyết được tìm thấy. \n Sử dụng các công cụ và tập lệnh kiểm tra tự động để hợp lý hóa các quy trình kiểm tra nếu có. \n Tiến hành thử nghiệm thăm dò để phát hiện các vấn đề không lường trước được và đảm bảo phạm vi thử nghiệm toàn diện.",
+            "Tốt nghiệp Đại học chuyên ngành Khoa học Máy tính, Kỹ thuật phần mềm hoặc lĩnh vực liên quan. \n Có ít nhất 3 năm kinh nghiệm về kiểm soát chất lượng phần mềm hoặc kiểm tra phần mềm \n Kiến thức vững chắc về các phương pháp, công cụ và phương pháp kiểm thử phần mềm tốt nhất. \n Có khả năng viết kế hoạch kiểm thử và trường hợp kiểm thử rõ ràng, ngắn gọn và toàn diện. \n Có kinh nghiệm với các công cụ tự động hóa thử nghiệm và ngôn ngữ kịch bản (ví dụ: Selenium, Python, v.v.). \n Kỹ năng phân tích và giải quyết vấn đề xuất sắc với sự chú ý sâu sắc đến từng chi tiết. \n Kỹ năng giao tiếp hiệu quả, cả bằng lời nói và văn bản, cùng với khả năng cộng tác với các nhóm đa chức năng. \n Khả năng làm việc độc lập và ưu tiên các nhiệm vụ một cách hiệu quả trong môi trường có nhịp độ nhanh.",
+            "https://www.youtube.com/watch?v=RDpcHAGZ0XI",
+            "https://www.youtube.com/watch?v=M8qQTbUgRfc",
+            "15,000,000",
+            "25,000,000",
+            "23", // From Age
+            "1", // select Exp
+            "2", // input Exp From
+            "6", // input Exp To
+            "3", // select Level_ID
+            "3", // select Degree_ID
+            "2 Months",
+            "Monday To Friday: 8h30 - 17h30",
+            "Learning skill from platform Udemy, Coursera",
+            "Friendly, Leader Manager Nice",
+            "Bonus Attractive 13th, Review Salary base on 6 month / year",
+            "With company rule",
+            "20 days / years"
         );
         onWorkWithhManageJob.JobOnPending(
-              "Nhân viên dịch vụ khách hàng/ Customer Service Staff",
-              "css-02",
-              "650", // option value location 4, 8, 76, 64, 781, 281, 240, 241, 75, 650, 651, 62, 78, 26, 71, 50, 1042, 511, 900, 1064, 61, 67, 59, 19, 351, 39, 320, 31, 780, 18, 321, 901, 58, 77, 60, 1069, 1070, 1071, 1072
-              "- Theo dõi mẫu, giao tiếp và làm việc tốt với các bộ phận liên quan.\n - Tư vấn và hỗ trợ khách hàng về sản phẩm của công ty \n - Quản lý thông tin khách hàng và cập nhật hồ sơ khách hàng \n - Xử lý các yêu cầu của khách hàng và giải quyết các vấn đề liên quan \n - Hỗ trợ hoạt động phát triển kinh doanh \n - Tham gia vào xây dựng và nâng cao quy trình chăm sóc khách hàng \n - Các công việc khác theo yêu cầu của Quản lý.",
-              "- Trình độ: Tốt nghiệp Cao đẳng, Đại học trở lên các ngành Marketing, Kinh tế \n- Quản trị hoặc các ngành có liên quan. \n- Có kinh nghiệm 2 - 3 năm ở vị trí tương đương. \n- Giao tiếp Tiếng Anh thành thạo \n- Kỹ năng giao tiếp và phối hợp tốt với các bộ phận liên quan \n- Khả năng nắm bắt và xử lý tình huống tốt \n- Sử dụng vi tính văn phòng thành thạo \n- Có khả năng làm việc độc lập và làm việc nhóm tốt \n- Cẩn thận, chính xác và tỉ mỉ trong công việc",
-              "25,000,000",
-              "50,000,000",
-              "23", // From Age
-              "1", // select Exp
-              "4", // input Exp From
-              "7", // input Exp To
-              "3", // select Level_ID
-              "3", // select Degree_ID
+            "Nhân viên dịch vụ khách hàng/ Customer Service Staff",
+            "css-02",
+            "650", // option value location 4, 8, 76, 64, 781, 281, 240, 241, 75, 650, 651, 62, 78, 26, 71, 50, 1042, 511, 900, 1064, 61, 67, 59, 19, 351, 39, 320, 31, 780, 18, 321, 901, 58, 77, 60, 1069, 1070, 1071, 1072
+            "- Theo dõi mẫu, giao tiếp và làm việc tốt với các bộ phận liên quan.\n - Tư vấn và hỗ trợ khách hàng về sản phẩm của công ty \n - Quản lý thông tin khách hàng và cập nhật hồ sơ khách hàng \n - Xử lý các yêu cầu của khách hàng và giải quyết các vấn đề liên quan \n - Hỗ trợ hoạt động phát triển kinh doanh \n - Tham gia vào xây dựng và nâng cao quy trình chăm sóc khách hàng \n - Các công việc khác theo yêu cầu của Quản lý.",
+            "- Trình độ: Tốt nghiệp Cao đẳng, Đại học trở lên các ngành Marketing, Kinh tế \n- Quản trị hoặc các ngành có liên quan. \n- Có kinh nghiệm 2 - 3 năm ở vị trí tương đương. \n- Giao tiếp Tiếng Anh thành thạo \n- Kỹ năng giao tiếp và phối hợp tốt với các bộ phận liên quan \n- Khả năng nắm bắt và xử lý tình huống tốt \n- Sử dụng vi tính văn phòng thành thạo \n- Có khả năng làm việc độc lập và làm việc nhóm tốt \n- Cẩn thận, chính xác và tỉ mỉ trong công việc",
+            "25,000,000",
+            "50,000,000",
+            "23", // From Age
+            "1", // select Exp
+            "4", // input Exp From
+            "7", // input Exp To
+            "3", // select Level_ID
+            "3", // select Degree_ID
         )
         onWorkWithhManageJob.JobOnInactive()
+        onWorkWithhManageJob.JobOnExpired(
+            "Sales Admin/ Client Operation Staff",
+            "QAL123",
+            "8", // option value location 4, 8, 76, 64, 781, 281, 240, 241, 75, 650, 651, 62, 78, 26, 71, 50, 1042, 511, 900, 1064, 61, 67, 59, 19, 351, 39, 320, 31, 780, 18, 321, 901, 58, 77, 60, 1069, 1070, 1071, 1072
+            "- Work directly with Suppliers / Brands to follow the promotion plan, promotion budget montly/yearly \n- Work directly with Sales Department to deploy, track promotions, POSM, activation. \n- Follow the stock, inventory for expired date of products, suggest to clear products \n- Forecast sales plan with sales team, inventory management in monthly \n- Handling damaged, expired goods.",
+            "-Have University certificate of economic, marketing, Bussiness Administration \n- English proficiency is required between daily conversations with suppliers and BOD members of our company. \n- Have experience of co-working with sales team/ working with clients in FMCG scopes \n- Priority for fesh student, willing to learn",
+            "https://www.youtube.com/watch?v=RDpcHAGZ0XI",
+            "https://www.youtube.com/watch?v=M8qQTbUgRfc",
+            "23", // From Age
+            "1", // select Exp
+            "2", // input Exp From
+            "5", // input Exp To
+            "4", // select Level_ID
+            "3", // select Degree_ID
+            "2 Months",
+            "Monday To Friday: 8h30 - 17h30",
+            "Learning skill from platform Udemy, Coursera",
+            "Friendly, Leader Manager Nice",
+            "Bonus Attractive 13th, Review Salary base on 6 month / year",
+            "With company rule",
+            "20 days / years"
+        )
     });
 })
--- cypress/support/page_objects_emp/Dashboard/WorkingWithManageJob.js
+++ cypress/support/page_objects_emp/Dashboard/WorkingWithManageJob.js
@@ -131,8 +131,9 @@ export class workWithhManageJob {
         let dateAssert = `${futureDay}/${fututeMonth}/${futureYear}`;
 
         cy.get("#JOB_LASTDATE").click();
+
         // Check if the future month is already displayed
-        cy.get(".monthselect").then(($monthSelect) => {
+        cy.get('.monthselect').then(($monthSelect) => {
             if (!$monthSelect.val().includes(fututeMonth)) {
                 // If not, click the "Next" button until the desired month is selected
                 cy.get('.table-condensed thead tr th[class="next available"]').click({ multiple: true, force: true })
@@ -249,6 +250,10 @@ export class workWithhManageJob {
         cy.wait(2000)
         cy.get('div[class="form-group form-submit"]').find("#btnSave").click(); // click confirm
         cy.get('div[class="jconfirm-buttons"] button').click(); // close popup confirm
+
+        cy.get('table tbody tr td .list-manipulation li a[title="Tạm dừng đăng"]').eq(0).click();
+        cy.get('.jconfirm-buttons').find('button').contains('Đồng ý').click(); //
+        cy.get('.jconfirm-buttons').find('button').contains('ok').click(); // đồng ý hạ job
     }
 
     JobOnPending(titleJob, jobCode_Optional, location, jobDescription, jobRequirements, salaryForm, salaryTo, job_FromAge, job_IsExp, job_FromExp, job_ToExp, Level_Id, Degree_Id) {
@@ -584,23 +589,346 @@ export class workWithhManageJob {
     }
 
     JobOnInactive() {
-        // Click job Inactive
         cy.get('ul[class="tabslet-tab"] li').eq(2).click();
         cy.get('ul[class="list-check"] li[class="view-posting-detail active"] a').click();
-        cy.get('#dtail').click({ force: true })  // click btn details 
-        cy.get('#copy_multi_job').click() // click copy Btn
+        cy.get('#dtail').click({ force: true }); // click btn details
+        cy.get('#copy_multi_job').click(); // click copy Btn
         cy.get('.modal-body .button a').click();
 
-        cy.get('table tbody tr td input').eq(0).click()
-        cy.get('#copy_multi_job').click() // click copy Btn
+        cy.get('table tbody tr td input').eq(0).click();
+        cy.get('#copy_multi_job').click(); // click copy Btn
         cy.get('ul[class="tabslet-tab"] li').eq(2).click();
-        cy.get('#copy_multi_job') // click copy Btn after choose data to copy
+        cy.get('#copy_multi_job').click(); // click copy Btn after choose data to copy
+        cy.get('.modal-body .button a').click();
 
-        cy.get('.export-file a').click() // click export file
+        cy.get('.export-file a').click(); // click export file
 
-        cy.get('table tbody tr td ul[class="list-manipulation"]').eq(0).find('a[title="Chi tiết"]').click()
+        // interact with table
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(0)
+            .find('a[title="Chi tiết"]')
+            .click();
+        cy.go('back');
+
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(1)
+            .find('a[title="Đăng lại việc làm này"]')
+            .click();
+        cy.get('.jconfirm-buttons').find('button').contains('Đồng ý').click(); //
+        cy.get('.jconfirm-buttons').find('button').contains('ok').click(); // đăng lại job
+
+        cy.get('table tbody tr td .list-manipulation li a[title="Tạm dừng đăng"]').eq(0).click();
+        cy.get('.jconfirm-buttons').find('button').contains('Đồng ý').click(); //
+        cy.get('.jconfirm-buttons').find('button').contains('ok').click(); // đồng ý hạ job
+
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(2)
+            .find('a[title="Nhân bản"]')
+            .click();
+        cy.go("back");
+
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(3)
+            .find('a[title="Xuất file hồ sơ ứng tuyển"]')
+            .click();
     }
 
-    JobOnExpired() { }
+    JobOnExpired(titleJob, jobCode_Optional, location, jobDescription, jobRequirements, linkYoutube, linkYoutube_1, job_FromAge, job_IsExp, job_FromExp, job_ToExp, Level_Id, Degree_Id, job_Probation, job_WorkTime, job_TrainOpportunity, job_Collegue, job_Benefit, job_AddSalary, job_Annualleave) {
+        cy.get('ul[class="tabslet-tab"] li').eq(3).click(); // click Job expired
+
+        cy.get('ul[class="list-check"] li[class="view-posting-detail active"] a').click();
+        cy.get('#dtail').click({ force: true }); // click btn details
+        cy.get('#copy_multi_job').click(); // click copy Btn
+        cy.get('.modal-body .button a').click();
+
+        cy.get('table tbody tr td input').eq(0).click();
+        cy.get('#copy_multi_job').click(); // click copy Btn
+        cy.get('ul[class="tabslet-tab"] li').eq(3).click(); // click Job expired
+        cy.get('#copy_multi_job').click(); // click copy Btn after choose data to copy
+        cy.get('.modal-body .button a').click();
+
+        cy.get('.export-file a').click(); // click export file
+
+        // interact with table
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(0)
+            .find('a[title="Chi tiết"]')
+            .click();
+        cy.go('back');
+
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(1)
+            .find('a[title="Nhân bản"]')
+            .click();
+        cy.go('back');
+
+        // this function is re-write job post
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(2)
+            .find('a[title="Đăng lại việc làm này"]')
+            .click();
+        // copy All code from function Job On Hiring
+        //form post Job
+        cy.get("#job_title")
+            .clear()
+            .type(`${titleJob}`)
+            .should("have.value", `${titleJob}`);
+        cy.get("#job_code")
+            .clear()
+            .type(`${jobCode_Optional}`)
+            .should("have.value", `${jobCode_Optional}`); // optional not required (Job Code)
+        cy.get('div[id="select_industry_db_chosen"]').then(
+            ($listIndustries) => {
+                // check if field industry is empty, we start to click some industry
+                if ($listIndustries.val().length > 0) {
+                    cy.get('div[id="select_industry_db_chosen"]').click();
+                    cy.get('.chosen-results li')
+                        .contains('CNTT - Phần mềm')
+                        .click({ ctrlKey: true })
+                    cy.get('.chosen-results li')
+                        .contains('Quản lý chất lượng (QA/QC)')
+                        .click({ ctrlKey: true });
+                    cy.get('body').click({ force: true }); // click body tag to off show list industry
+                } else {
+                    // If options are already selected, delete one industry then click a new one
+                    cy.get('.search-choice').eq(0).find('a').click();
+                    cy.get('div[id="select_industry_db_chosen"]').click();
+                    cy.get('.chosen-results li')
+                        .contains('Bán lẻ / Bán sỉ')
+                        .click({ ctrlKey: true });
+                    cy.get('body').click({ force: true });
+                }
+            });
+        cy.get('body').click({ force: true }); // click body tag to off show list industry 
+
+        cy.get("#post_job_location")
+            .find('select[name="LOCATION_ID[]"]')
+            .select(`${location}`);
+
+        cy.get('#post_job_location div[class="chosen-container chosen-container-multi"]').then(($listAddressWorkLocation) => {
+            if ($listAddressWorkLocation.length === 0) {
+                // check if location address is empty, we start to add new address location
+                cy.get("#post_job_location").find("label a").click(); // add address location
+                cy.get("#location_id").select("650"); // option value location 4, 8, 76, 64, 781, 281, 240, 241, 75, 650, 651, 62, 78, 26, 71, 50, 1042, 511, 900, 1064, 61, 67, 59, 19, 351, 39, 320, 31, 780, 18, 321, 901, 58, 77, 60, 1069, 1070, 1071, 1072
+                cy.get("#sldistrict").select("311");
+                cy.get("#address")
+                    .clear()
+                    .type("251 Nguyễn Thương An")
+                    .should("have.value", "251 Nguyễn Thương An");
+                cy.get("#insert_location_account").click();
+                cy.get('div[class="chosen-container chosen-container-multi"]')
+                    .eq(1)
+                    .click();
+                cy.get("#post_job_location .chosen-drop .chosen-results")
+                    .find("li")
+                    .eq(0)
+                    .click();
+            } else {
+                cy.get('div[class="chosen-container chosen-container-multi"]')
+                    .eq(1)
+                    .click();
+                cy.get("#post_job_location .chosen-drop .chosen-results")
+                    .find("li")
+                    .eq(0)
+                    .click();
+            }
+        });
+
+        // Job Description Iframe
+        cy.get("iframe.cke_wysiwyg_frame")
+            .eq(0)
+            .then(($iframe) => {
+                const iframeBody = $iframe.contents().find("body");
+                cy.wrap(iframeBody)
+                    .clear() // Clear any existing text
+                    .type(`${jobDescription}`);
+            });
+
+        // Job Requirement Iframe
+        cy.get("iframe.cke_wysiwyg_frame")
+            .eq(1)
+            .then(($iframe) => {
+                const iframeBody = $iframe.contents().find("body");
+                cy.wrap(iframeBody).clear().type(`${jobRequirements}`); // Clear any existing text
+            });
+        // link video present job
+        cy.get("#strVideoRecruiment")
+            .clear()
+            .type(`${linkYoutube}`)
+            .should("have.value", `${linkYoutube}`);
+        cy.get("#strVideoRecruiment2")
+            .clear()
+            .type(`${linkYoutube_1}`)
+            .should("have.value", `${linkYoutube_1}`);
+
+        // range salary
+        cy.get('#job_salaryunit').then(optionValue => {
+            const choose_value = optionValue.val(); // lay gia tri the option trong lua chon select
+            if (choose_value === 'vnd') {
+                cy.get('#salary_from')
+                    .clear()
+                    .type('13,000,000')
+                    .should('have.value', '13,000,000');
+                cy.get('#salary_to')
+                    .clear()
+                    .type('18,000,000')
+                    .should('have.value', '18,000,000');
+            } else {
+                cy.get('#salary_from')
+                    .clear()
+                    .type('1500')
+                    .should('have.value', '1,500');
+                cy.get('#salary_to')
+                    .clear()
+                    .type('2500')
+                    .should('have.value', '2,500');
+            }
+        })
+
+        // Worktype
+        cy.get('#job_type1').click();
+        cy.get('#job_type3').click();
+
+        // choose day to receive Resume
+        let date = new Date();
+        date.setDate(date.getDate() + 30);
+        let futureDay = date.getDate();
+        let fututeMonth = (date.getMonth() + 1).toString().padStart(2, "0"); // Note: Months are zero-indexed in JavaScript
+        let futureYear = date.getFullYear();
+        let dateAssert = `${futureDay}/${fututeMonth}/${futureYear}`;
+
+        cy.get('#JOB_LASTDATE').click();
+
+        // Check if the future month is already displayed
+        cy.get('.monthselect').then(($monthSelect) => {
+            if (!$monthSelect.val().includes(fututeMonth)) {
+                // If not, click the "Next" button until the desired month is selected
+                cy.get('.table-condensed thead tr th[class="next available"]').click({ multiple: true, force: true })
+            }
+        })
+
+        // Find and click the future date
+        cy.get('.calendar-table tbody td').each(($dayClick) => {
+            if ($dayClick.text() === futureDay.toString() && $dayClick.hasClass('available')) {
+                cy.wrap($dayClick).click();
+                // cy.get('#JOB_LASTDATE').should('have.value', `${dateAssert}`)
+                return false; // Exit the loop once the desired element is clicked
+            }
+        });
+        // Benefit
+        let i = 1;
+        while (i < 15) {
+            cy.get(`input[name="BENEFIT_ID[]"][value=${i}]`).click();
+            i++;
+        }
+
+        // BASIC REQUIREMENT
+        cy.get('#JOB_FROMAGE')
+            .clear()
+            .type(`${job_FromAge}`)
+            .should('have.value', `${job_FromAge}`);
+        cy.get('#JOB_ISEXPERIENCE')
+            .select(`${job_IsExp}`)
+            .should('have.value', `${job_IsExp}`);
+        cy.get('#JOB_FROMEXPERIENCE')
+            .clear()
+            .type(`${job_FromExp}`)
+            .should('have.value', `${job_FromExp}`);
+        cy.get('#JOB_TOEXPERIENCE')
+            .clear()
+            .type(`${job_ToExp}`)
+            .should('have.value', `${job_ToExp}`);
+        cy.get('#LEVEL_ID')
+            .select(`${Level_Id}`)
+            .should('have.value', `${Level_Id}`);
+        cy.get('#DEGREE_ID')
+            .select(`${Degree_Id}`)
+            .should('have.value', `${Degree_Id}`);
+        // CONTACT INFORMATION (SKip)
+        // EMPLOYER'S PRIORITIZED CRITERIA IN SCREENING( RECRUITMENT INFORMATION ) (Skip)
+        // OTHER INFO (OPTIONAL)
+        cy.get('div[class="form-wrap other-information-wrap"]').find("a").click();
+        cy.get('#JOB_PROBATIONTIME')
+            .clear()
+            .type(`${job_Probation}`)
+            .should('have.value', `${job_Probation}`);
+        cy.get('#JOB_WORKTIME')
+            .clear()
+            .type(`${job_WorkTime}`)
+            .should('have.value', `${job_WorkTime}`);
+        cy.get('#JOB_TRAINOPPORTUNITY')
+            .clear()
+            .type(`${job_TrainOpportunity}`)
+            .should('have.value', `${job_TrainOpportunity}`);
+        cy.get('#JOB_COLLEAGUE')
+            .clear()
+            .type(`${job_Collegue}`)
+            .should('have.value', `${job_Collegue}`);
+        cy.get('#JOB_ADDSALARY')
+            .clear()
+            .type(`${job_Benefit}`)
+            .should('have.value', `${job_Benefit}`);
+        cy.get('#JOB_ADDSALARY')
+            .clear()
+            .type(`${job_AddSalary}`)
+            .should('have.value', `${job_AddSalary}`);
+        cy.get('#JOB_ANNUALLEAVE')
+            .clear()
+            .type(`${job_Annualleave}`)
+            .should('have.value', `${job_Annualleave}`);
+
+
+
+        // Click Btn Save and Wait to Page Pending Jobs
+        cy.get('div[class="item-add-info"]')
+            .find('button[class="button-add-update btn-post"]')
+            .click();
+
+
+        // click job information
+        cy.get('div[class="jobs-posting-detail-bottom"] ul')
+            .find("li")
+            .eq(1)
+            .click();
+
+        // click autoresponse
+        cy.get('div[class="jobs-posting-detail-bottom"] ul')
+            .find("li")
+            .eq(2)
+            .click();
+        cy.get('a[class="btn-gradient btn-edit-email"]').click();
+        cy.get("#slAutoReply").select("294976");
+        cy.wait(1000);
+        cy.get('div[class="jobs-posting-modal jobs-posting-16-modal fancybox-content"] button[name="save"]').click();
+
+        // click resume tag management
+        cy.get('div[class="jobs-posting-detail-bottom"] ul')
+            .find("li")
+            .eq(3)
+            .click();
+
+        // click Btn post job
+        cy.get('ul[class="list-action"]').find("li").eq(1).click();
+
+        // choose service to post jobs
+        cy.get('div[class="table-jobs-waiting"]')
+            .find('#c_pack_3172917_1126_4532276') // 
+            .click(); // click service
+        cy.get('div[class="button"]')
+            .find('a[class="btn-gradient btn-posting-jobs"]')
+            .click(); // click btn postjobs
+        cy.get('div[class="form-group form-submit"]').find("#btnSave").click(); // click confirm
+        cy.get('div[class="jconfirm-buttons"] button').click(); // close popup confirm
+
+        cy.get('table tbody tr td .list-manipulation li a[title="Tạm dừng đăng"]').eq(0).click();
+        cy.get('.jconfirm-buttons').find('button').contains('Đồng ý').click();
+        cy.get('.jconfirm-buttons').find('button').contains('ok').click(); // đồng ý hạ job
+
+        cy.get('ul[class="tabslet-tab"] li').eq(3).click(); // click Job expired
+        cy.get('table tbody tr td ul[class="list-manipulation"]')
+            .eq(3)
+            .find('a[title="Xuất file hồ sơ ứng tuyển"]')
+            .click();
+    }
 }
 export const onWorkWithhManageJob = new workWithhManageJob();
