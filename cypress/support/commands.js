fe59623f8765ea90b9ca2c46a43e69a93ddac6f9
Hà Nguyễn Phương Nam <73868730+flames239@users.noreply.github.com>
2024-08-26 13:34:21 +0700

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,11 +23,45 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('openHomePage',() => {
-    cy.visit('/')
-})
+Cypress.Commands.add("openHomePage", () => {
+  cy.visit("/");
+});
 
-Cypress.Commands.add('openHomePageEmp',() => {
-    cy.visit('/vi/employers')
-})
+Cypress.Commands.add("openHomePageEmp", () => {
+  cy.visit(Cypress.env("EmpBaseUrl"));
+});
 
+Cypress.Commands.add("selectIndustry", (industries = []) => {
+  cy.get('div[id="select_industry_db_chosen"]').click();
+  industries.forEach((industry) => {
+    cy.get(".chosen-results li").contains(industry).click({ ctrlKey: true });
+  });
+  cy.get("body").click({ force: true }); // click body to close dropdown
+});
+
+Cypress.Commands.add("fillIframe", (selector, content) => {
+  cy.get(selector).then(($iframe) => {
+    const iframeBody = $iframe.contents().find("body");
+    cy.wrap(iframeBody).clear().type(content);
+  });
+});
+
+Cypress.Commands.add("dayFill", (selector, days, month, years) => {
+  cy.get(selector)
+    .find('input[class="dtpicker-compValue"]')
+    .eq(0)
+    .type(days)
+    .should("be.visible", "have.value", days);
+  cy.get(selector)
+    .find('input[class="dtpicker-compValue"]')
+    .eq(1)
+    .type(month)
+    .should("be.visible", "have.value", month);
+  cy.get('div[class="dtpicker-comp year"]')
+    .find('input[class="dtpicker-compValue"]')
+    .type(years)
+    .should("be.visible", "have.value", years);
+  cy.get(selector)
+    .find('a[class="dtpicker-button dtpicker-buttonSet"]')
+    .click();
+});
